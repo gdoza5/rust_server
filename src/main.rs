557fe3c83168088ff84d9f4bbac7b013b1fc2103
Gerardo Mendoza Munoz <gdoza@Gerardos-MacBook-Air.local>
2024-01-23 14:44:44 -0700

    server work

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 fn main() {
-    let servder = Server::new("127.0.0.1:8080") ;
+    let server = Server::new("127.0.0.1:8080") ;
     server.run();
 }
 struct Server {
     addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    } 
+
+    fn run(self) {
+
+    }  
 }
